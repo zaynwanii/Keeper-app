3311325cf177df9f2976c32bd6b6950642387777
zaynwanii <84954279+zaynwanii@users.noreply.github.com>
2024-05-23 07:17:21 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.createRoot( document.getElementById("root")).render(<App />);
+
+
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
